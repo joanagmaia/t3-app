a2b8a382880b3cffff9aaac655cb6dbf73d5e393
Joana Maia <joana@crowd.dev>
2024-01-24 17:48:26 +0000

    Font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ const PostView = (props: PostWithUser) => {
           <span>·</span>
           <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
